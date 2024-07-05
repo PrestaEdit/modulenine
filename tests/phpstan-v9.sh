6ed3181eee9e5ade56845be2fe177e1975a56bfd
Jonathan Danse <j.danse@prestaedit.com>
2024-07-05 14:53:41 +0200

    1.7/8

--- tests/phpstan-v9.sh
+++ tests/phpstan-v9.sh
@@ -6,23 +6,25 @@ set -e
 # Docker images prestashop/prestashop may be used, even if the shop remains uninstalled
 echo "Pull PrestaShop files (Tag ${PS_VERSION})"
 
-docker rm -f temp-ps || true
-docker volume rm -f ps-volume || true
+docker rm -f ps9-php8 || true
+docker volume rm -f ps9-php8 || true
 
-docker run -tid --rm -v ps-volume:/var/www/html --name temp-ps prestashop/prestashop:$PS_VERSION
+docker run -tid --rm -v ps9-php8:/var/www/html --name ps9-php8 prestaedit/prestashop:$PS_VERSION
+
+docker exec -i ps9-php8 php -v
 
 # Clear previous instance of the module in the PrestaShop volume
 echo "Clear previous module"
 
-docker exec -t --user root temp-ps sh -c 'find /var/www/html/modules/modulenine -type f -exec rm {} +'
+docker exec -tid ps9-php8 rm -rf /var/www/html/modules/modulenine
 
 # Run a container for PHPStan, having access to the module content and PrestaShop sources.
 # This tool is outside the composer.json because of the compatibility with PHP 5.6
 echo "Run PHPStan using phpstan-${PS_VERSION}.neon file"
 
-docker run --rm --volumes-from temp-ps \
+docker run --rm --volumes-from ps9-php8 \
        -v $PWD:/var/www/html/modules/modulenine \
        -e _PS_ROOT_DIR_=/var/www/html \
-       --workdir=/var/www/html/modules/modulenine phpstan/phpstan:0.12.54 \
+       --workdir=/var/www/html/modules/modulenine ghcr.io/phpstan/phpstan:nightly-php8.1 \
        analyse \
-       --configuration=/var/www/html/modules/modulenine/tests/php/phpstan/phpstan-$PS_VERSION.neon
+       --configuration=/var/www/html/modules/modulenine/tests/phpstan/phpstan-$PS_VERSION.neon
