a45ae45f2fa32ffe2f2485f5146ce54adf0b20fa
Jonathan Danse <j.danse@prestaedit.com>
2025-06-10 16:37:04 +0200

    PHP 8.4 & PrestaShop-9.0.0-classic

---
+++ tests/phpstan-v9-classic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PS_VERSION=$1
+
+set -e
+
+# Docker images prestashop/prestashop may be used, even if the shop remains uninstalled
+echo "Pull PrestaShop files (Tag ${PS_VERSION})"
+
+docker rm -f ps9-php8 || true
+docker volume rm -f ps9-php8 || true
+
+docker run -tid --rm -v ps9-php8:/var/www/html --name ps9-php8 prestashop/prestashop:$PS_VERSION
+
+docker exec -i ps9-php8 php -v
+
+# Run a container for PHPStan, having access to the module content and PrestaShop sources.
+# This tool is outside the composer.json because of the compatibility with PHP 5.6
+echo "Run PHPStan using phpstan-${PS_VERSION}.neon file"
+
+docker run --rm --volumes-from ps9-php8 \
+       -v $PWD:/var/www/html/modules/modulenine \
+       -e _PS_ROOT_DIR_=/var/www/html \
+       --workdir=/var/www/html/modules/modulenine ghcr.io/phpstan/phpstan:nightly-php8.1 \
+       analyse \
+       --configuration=/var/www/html/modules/modulenine/tests/phpstan/phpstan-$PS_VERSION.neon
